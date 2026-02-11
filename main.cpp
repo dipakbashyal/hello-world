fd0d84f2c8d723e33d98fa4e267dc43dcfcad8e8
Dipak Bashyal <dipakbashyal100@gmail.com>
2026-02-11 15:09:14 -0500

    Add user input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
 
 int main() {
+  // Var initialization
+  std::string userInput;
+
   std::cout << "Hello world!" << std::endl;
+  std::cout << "What is your name?" << std::endl;
+  std::getline(std::cin, userInput);
+  std::cout << "Hello " << userInput << "!" << std::endl;
+  
   return 0;
 }
