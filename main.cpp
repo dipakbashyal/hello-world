8a36876c60e5cadbc08620004fda91c534d05c01
dipakbashyal <dipakbashyal100@gmail.com>
2026-02-12 17:47:11 -0500

    Simplify main function by removing user input
    Removed user input functionality and string initialization.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,5 @@
 #include <iostream>
-#include <string>
-
 int main() {
-  // Var initialization
-  std::string userInput;
-
-  std::cout << "Hello world!" << std::endl;
-  std::cout << "What is your name?" << std::endl;
-  std::getline(std::cin, userInput);
-  std::cout << "Hello " << userInput << "!" << std::endl;
-  
-  return 0;
+std::cout << "Hello world!" << std::endl;
+return 0;
 }
